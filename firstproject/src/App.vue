6b84b63da0f69e6a6c4aff9e822ab742ee9c0366
xsheng1992 <a415441091@hotmail.com>
2017-04-18 16:49:54 +0800

    add vuejs-demo folder

---
+++ firstproject/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <h4>This is a grid component built with vue-webpack-simple scaffolding!</h4>
+    <div class="search-bar">
+      search:
+      <input v-model="query" class="query-input">
+    </div>
+    <simple-table 
+      v-bind:tTitle="headList"
+      v-bind:tData="fruitList"
+      v-bind:tQuery="query"
+    ></simple-table>
+  </div>
+</template>
+
+<script>
+import simpleTable from './component/simpleTable.vue';
+
+export default {
+  data () {
+    return {
+      query: '',
+      headList: ['name','price'],
+      fruitList: [
+        {name: "Apple", price: "6.00"},
+        {name: "Orange", price: "8.00"},
+        {name: "Pear",   price: "6.00"},
+        {name: "Strawberry", price: "12.00"}
+      ]
+    }
+  },
+  components: {
+    'simple-table': simpleTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Microsoft Yahei', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #333;
+  margin: 60px auto;
+  max-width: 800px;
+}
+
+.query-input {
+  width: 500px;
+  height: 30px;
+  outline: none;
+  border: 1px solid #999;
+  padding: 5px 15px;
+}
+.query-input:focus {
+  border-color: #42b983;
+}
+</style>
---
+++ firstproject/src/component/simpleTable.vue
@@ -0,0 +1,96 @@
+<template>
+  <table class="f-table">
+    <thead>
+      <tr>
+        <th v-for="item in tTitle">{{item}}</th>
+      </tr>
+    </thead>
+    <transition-group 
+      tag="tbody"
+      v-on:before-enter="beforeEnter"
+      v-on:enter="enter"
+      v-on:leave="leave"
+    >
+      <tr v-for="(row, index) in computedList" v-bind:key="row.name" v-bind:data-index="index">
+        <td v-for="item in tTitle">{{row[item]}}</td>
+      </tr>
+    </transition-group>
+  </table>
+</template>
+
+<script>
+//import Velocity from 'velocity-animate'
+import Velocity from 'velocity'
+
+var hasString = function(origin, string) {
+  return origin.toLowerCase().indexOf(string.toLowerCase()) !== -1;
+};
+
+export default {
+  props: {
+    tTitle: Array,
+    tData: Array,
+    tQuery: String
+  },
+  computed: {
+    computedList: function() {
+      var vm = this;
+      return this.tData.filter(function(row){
+        return hasString(row.name, vm.tQuery) || hasString(row.price, vm.tQuery);
+      });
+    }
+  },
+  methods: {
+    beforeEnter: function(el) {
+      el.style.opacity = 0;
+      el.style.height = 0;
+    },
+    enter: function(el, done) {
+      var delay = el.dataset.index*100;
+      setTimeout(function(){
+        Velocity(el,
+          {opacity:1, height: '31px'},
+          {complete: done}
+        );
+      }, delay);
+    },
+    leave: function(el, done) {
+      var delay = el.dataset.index*100;
+      setTimeout(function(){
+        Velocity(el,
+          {opacity:0, height: 0},
+          {complete: done}
+        );
+      }, delay);
+    }
+  }
+  
+}
+</script>
+
+<style>
+.f-table {
+  width: 600px;
+  margin: 20px auto;
+  border: 1px solid #bbb;
+  border-collapse: collapse;
+}
+.f-table>thead>tr>th, .f-table>tbody>tr>td {
+  border: 1px solid #bbb;
+}
+.f-table>thead>tr>th {
+  width: 50%;
+  padding: 10px 15px;
+  font-size: 14px;
+  font-weight: normal;
+  color: #fff;
+  background-color: #42b983;
+}
+.f-table>tbody>tr>td {
+  padding: 5px 15px;
+  line-height: 20px;
+}
+.f-table>tbody>tr:nth-child(odd) {
+  background-color: #eee;
+}
+</style>
